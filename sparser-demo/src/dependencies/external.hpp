444bfb25f63ca3bf176192dcd6a1d4c1dc98a2c5
maroctamorg <guimaraesneto.marcelo@gmail.com>
2023-02-07 20:20:50 +0200

    sparser-demo base

---
+++ sparser-demo/src/dependencies/external.hpp
@@ -0,0 +1,16 @@
+#ifndef EXTERNAL_HPP
+#define EXTERNAL_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <limits>
+#include <memory>
+#include <utility>
+
+#endif
