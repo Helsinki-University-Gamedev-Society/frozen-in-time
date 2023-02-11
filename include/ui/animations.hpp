cff6b1e12564f0d7f7726ff2e2ef34a3e9a6126a
Sonny Jansson <sonnyjansson01@gmail.com>
2023-02-11 16:28:29 +0200

    Refactored how animations work, and added text animation

--- include/ui/animations.hpp
+++ include/ui/animations.hpp
@@ -2,27 +2,54 @@
 #define _UI_ANIMATIONS_
 
 #include <SDL.h>
+#include <SDL_render.h>
 
-template <typename T>
+#include "ui/text.hpp"
+
 class Animation {
-protected:
-    T current_state;
 public:
+    void start();
+    void update();
+    bool has_started();
+private:
     virtual void progress(double dt) = 0;
-    virtual bool is_finished() = 0;
-    T get_current_state() {return current_state;}
+private:
+    bool started = false;
+    double time_last_updated = SDL_GetPerformanceCounter();
 };
 
-class EaseOutExpoAnimation: public Animation<SDL_Point> {
+class EaseOutExpoAnimation: public Animation {
+private:
+    SDL_Point current_state;
+public:
+    EaseOutExpoAnimation(SDL_Point from, SDL_Point to, double time);
+
+    bool is_finished();
+    SDL_Point get_current_state() {return current_state;}
+private:
+    void progress(double dt);
 private:
     SDL_Point from;
     SDL_Point to;
+
     double total_time;
     double current_time = 0;
+};
+
+class TextAppearAnimation: public Animation {
 public:
-    EaseOutExpoAnimation(SDL_Point from, SDL_Point to, double time);
-    void progress(double dt);
+    TextAppearAnimation(Text text, double time);
+
     bool is_finished();
+    void render(SDL_Renderer *renderer, SDL_Point location);
+    pair<int, int> get_size();
+private:
+    void progress(double dt);
+private:
+    Text text;
+
+    double total_time;
+    double current_time = 0;
 };
 
  #endif // _UI_ANIMATIONS_
--- src/ui/animations.cpp
+++ src/ui/animations.cpp
@@ -1,18 +1,41 @@
 #include <cmath>
+#include <iostream>
+
+#include <SDL_rect.h>
+#include <SDL_render.h>
 
 #include "ui/animations.hpp"
 
-EaseOutExpoAnimation::EaseOutExpoAnimation(SDL_Point from, SDL_Point to, double time):
-    from(from),
-    to(to),
-    total_time(time) {}
+void Animation::start() {
+    started = true;
+}
+
+void Animation::update() {
+    double time_now = SDL_GetPerformanceCounter();
+    double delta_time = (time_now - time_last_updated) / (double) SDL_GetPerformanceFrequency();
+
+    if(started) {
+	progress(delta_time);
+    }
+
+    time_last_updated = time_now;
+}
+
+bool Animation::has_started() {
+    return started;
+}
+
+EaseOutExpoAnimation::EaseOutExpoAnimation(SDL_Point from, SDL_Point to, double time)
+    : from(from)
+    , to(to)
+    , total_time(time) {}
 
 void EaseOutExpoAnimation::progress(double dt) {
     current_time += dt;
     current_time = current_time > total_time ? total_time : current_time;
 
     double p_t = current_time / total_time;
-    double p_d = p_t == 1 ? 1 : 1 - pow(2, -10 * p_t);
+    double p_d = p_t >= 0.75 ? 1 : 1 - pow(2, -10 * p_t);
 
     current_state = SDL_Point{(int) (p_d * to.x + (1 - p_d) * from.x),
 			      (int) (p_d * to.y + (1 - p_d) * from.y)};
@@ -20,4 +43,28 @@ void EaseOutExpoAnimation::progress(double dt) {
 
 bool EaseOutExpoAnimation::is_finished() {
     return current_time >= total_time;
+}
+
+TextAppearAnimation::TextAppearAnimation(Text text, double time)
+    : text(text)
+    , total_time(time) {}
+
+void TextAppearAnimation::progress(double dt) {
+    current_time += dt;
+    current_time = current_time > total_time  ? total_time : current_time;
+}
+
+void TextAppearAnimation::render(SDL_Renderer *renderer, SDL_Point point) {
+    auto [w, h] = text.get_size();
+    SDL_Rect target{point.x, point.y, w, h};
+
+    // std::cout << "Width: " << w << ", height: " << h << std::endl;
+
+    if(current_time >= total_time) {
+	SDL_RenderCopy(renderer, text.get_texture(), NULL, &target);
+    }
+}
+
+pair<int, int> TextAppearAnimation::get_size() {
+    return text.get_size();
 }
