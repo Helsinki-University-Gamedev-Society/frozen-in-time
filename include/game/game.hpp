880c3bcad50a6362e3d98cf77752f146306e2705
Sonny Jansson <sonnyjansson01@gmail.com>
2023-02-13 22:11:37 +0200

    Moved game header files

---
+++ include/game/game.hpp
@@ -0,0 +1,45 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  game.hpp
+ *
+ *    Description:  game definitions 
+ *
+ *        Version:  1.0
+ *        Created:  02/13/2023 08:51:00 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  maroctamorg (mg), marcelogn.dev@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "game/state.hpp"
+#include "ui/ui.hpp"
+
+class Game
+{
+public:
+	Game(UI* ui);
+        std::string Execute(Story, std::string command);
+private:
+	State state;
+	UI* ui;
+
+	std::string baseResponse;
+	
+	bool ExecuteBase(std::string command);
+	std::string ExecuteTent(std::string command);
+        std::string ExecuteDigsite(Story story, std::string command);
+
+        bool InventoryContains(string name);
+
+};
+
+
+#endif /* GAME_HPP */
---
+++ include/game/state.hpp
@@ -0,0 +1,57 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  state.hpp
+ *
+ *    Description:  state declarations
+ *
+ *        Version:  1.0
+ *        Created:  02/13/2023 08:39:13 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  maroctamorg (mg), marcelogn.dev@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <string>
+#include <vector>
+
+#include "ui/ui.hpp"
+
+struct InvItem
+{
+	std::string name;
+	std::string asset;
+	std::string value;
+};
+
+struct Inventory
+{
+	std::vector<InvItem> contents;
+	std::string List();
+};
+
+enum Scene
+{
+	TENT,
+	DIGSITE,
+};
+
+struct State
+{
+	Scene scene;
+	Story story;
+	Inventory inventory;
+	std::string track;
+
+        bool blake_talked_to = false;
+};
+
+
+#endif /* STATE_HPP */
